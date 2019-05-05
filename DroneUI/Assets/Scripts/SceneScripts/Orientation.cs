699b2f71b186e68d7f94488a15e110f11d846a5e
Tyler <bedert@iastate.edu>
2019-05-04 20:30:12 -0500

    initial project creation. Basic TCP socket setup.

---
+++ DroneUI/Assets/Scripts/SceneScripts/Orientation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Orientation : MonoBehaviour
+{
+
+    bool socketReady = false;                
+    TcpClient mySocket;
+    public NetworkStream NetStream;
+    StreamWriter sw;
+    StreamReader sr;
+    public string Host = "192.88.1.1";
+    public int Port = 8080;
+    public bool lightStatus;
+    //Start connection with Quad here
+    private void Awake()
+    {
+        Connect();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Send the char requesting the sensor data from drone. Drone responds, use data.
+    void Update()
+    {
+        while(NetStream.DataAvailable)
+        {
+            string data = ReadSocket();
+            Debug.Log(data);
+        }
+    }
+    private void Connect()
+    {
+        try
+        {
+            mySocket = new TcpClient(Host, Port);
+            NetStream = mySocket.GetStream();
+            sr = new StreamReader(NetStream);
+            sw = new StreamWriter(NetStream);
+            
+            socketReady = true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("Socket error:" + e);           
+        }
+
+    }
+    public string ReadSocket()
+    {                        
+        if (!socketReady)
+            return "";
+        if (NetStream.DataAvailable)
+            return sr.ReadLine();
+        return "No Data";
+    }
+}
